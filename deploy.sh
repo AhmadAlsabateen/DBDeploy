e72cebf16fbc1edcd2a6d84e912390e0a96b8a30
Ahmad Sabateen <ahmad2sabateen@gmail.com>
2021-09-07 01:18:18 +0300

    used digest instead of tag

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,16 @@ docker push $DOCKER_HUB_USERNAME/$DB_SERVICE
 
 
 
+#Digest
+DB_SERVICE_DIGEST=`docker inspect --format='{{index .RepoDigests 0}}' $DOCKER_HUB_USERNAME/$DB_SERVICE`
+
+
+#Fill Values
+sed 's/{image-value}/$DB_SERVICE_DIGEST/' db-service-template.yaml > db-service.yaml
+
 #Deploy
 kubectl apply -f db-service.yaml
+
 
 
 kubectl get deployments
